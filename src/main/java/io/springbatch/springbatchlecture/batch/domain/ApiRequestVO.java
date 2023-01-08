ba5e0a6c29685ea824c2e5fab0598cc727ed0312
eatnows <gagyasindev@gmail.com>
2023-01-08 16:15:24 +0900

    feat: 같은 날짜 중복 실행 방지와 파일 추출

--- src/main/java/io/springbatch/springbatchlecture/batch/domain/ApiRequestVO.java
+++ src/main/java/io/springbatch/springbatchlecture/batch/domain/ApiRequestVO.java
@@ -1,16 +1,23 @@
 package io.springbatch.springbatchlecture.batch.domain;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 
-@Getter
+@Data
 public class ApiRequestVO {
     private long id;
     private ProductVO productVO;
+    private ApiResponseVO apiResponseVO;
 
     @Builder
-    public ApiRequestVO(long id, ProductVO productVO) {
+    public ApiRequestVO(long id, ProductVO productVO, ApiResponseVO apiResponseVO) {
         this.id = id;
         this.productVO = productVO;
+        this.apiResponseVO = apiResponseVO;
+    }
+
+    public void setApiResponseVO(ApiResponseVO apiResponseVO) {
+        this.apiResponseVO = apiResponseVO;
     }
 }
